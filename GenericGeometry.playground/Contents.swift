17473294a4b4a1c34102bd6598d77907ebe2c4b2
Laurie Hufford <lauriehufford@gmail.com>
2019-03-28 14:37:18 +0100

    Embed project in a workspace

--- GenericGeometry.playground/Contents.swift
+++ GenericGeometry.playground/Contents.swift
@@ -1,19 +1,6 @@
-# 📐 Generic Geometry
+import Foundation
+import GenericGeometry
 
-A library for defining geometry of arbitrary, generic dimensions.
-
-Includes the following types:
-
-- `Point<Value>`
-- `Size<Value>`
-- `Rect<Value>`
-- `Edges<Value>`
-- `Corners<Value>`
-
-
-This allows you to do things like:
-
-```swift
 let maybeSize: Size<Int?> = Size(width: nil, height: 15)
 // w: nil, h: Optional(15)
 
@@ -25,5 +12,3 @@ let stringSize: Size<String> = concreteSize.map(String.init)
 
 let scaledSize = concreteSize.multipling(by: Size<Int>(width: 10, height: 100))
 // w: 9990, h: 1500
-
-```
