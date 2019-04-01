dcf8e1f31c4de4b936efeac0ba87e207e2091962
Laurie Hufford <lauriehufford@gmail.com>
2019-04-01 10:23:27 +0200

    Add a few unit tests

--- Tests/GenericGeometryTests.swift
+++ Tests/GenericGeometryTests.swift
@@ -6,20 +6,29 @@
 //
 
 import XCTest
+@testable import GenericGeometry
 
 class GenericGeometryTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testPoint() {
+        
+        let boolPoint = Point(x: true, y: false)
+        XCTAssertEqual(boolPoint.x, true)
+        XCTAssertEqual(boolPoint.y, false)
+        
+        let intPoint = Point(5)
+        XCTAssertEqual(intPoint.x, intPoint.y)
+        XCTAssertEqual(intPoint.y, 5)
+        
+        let dblPoint = Point<Double>(x: 0.5, y: 2)
+        
+        let scaledPoint = intPoint
+            .map(Double.init)
+            .zipWith(dblPoint, *)
+        XCTAssert(scaledPoint == Point<Double>(x: 2.5, y: 10))
+        
+        let zero = Point<Int>.zero
+        XCTAssertEqual(zero, Point<Int>(0))
     }
 
 }
